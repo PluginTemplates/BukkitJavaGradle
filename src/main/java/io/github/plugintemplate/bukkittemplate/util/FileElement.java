86b5e9c8c8b92798e188acabb0c91ff0eb3fa8d9
Hasan Demirtaş <utsukushihito@outlook.com>
2020-08-09 18:22:33 +0300

    Update FileElement.java

--- src/main/java/io/github/plugintemplate/bukkittemplate/util/FileElement.java
+++ src/main/java/io/github/plugintemplate/bukkittemplate/util/FileElement.java
@@ -3,15 +3,18 @@ package io.github.plugintemplate.bukkittemplate.util;
 import io.github.portlek.configs.CfgSection;
 import io.github.portlek.configs.Provided;
 import io.github.portlek.configs.bukkit.BkktSection;
+import io.github.portlek.configs.util.GeneralUtilities;
 import io.github.portlek.smartinventory.Icon;
 import io.github.portlek.smartinventory.InventoryContents;
 import io.github.portlek.smartinventory.event.abs.ClickEvent;
+import io.github.portlek.smartinventory.util.SlotPos;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
@@ -19,23 +22,33 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 
 @RequiredArgsConstructor
+@Getter
 public final class FileElement {
 
     @NotNull
     private final ItemStack itemStack;
 
-    private final int row;
+    @NotNull
+    private final SlotPos position;
 
-    private final int column;
+    public FileElement(@NotNull final ItemStack itemStack, final int row, final int column) {
+        this(itemStack, SlotPos.of(row, column));
+    }
 
     public FileElement(@NotNull final FileElement fileElement) {
-        this.itemStack = fileElement.itemStack;
-        this.row = fileElement.row;
-        this.column = fileElement.column;
+        this(fileElement.itemStack, fileElement.position);
+    }
+
+    public int row() {
+        return this.position.getRow();
+    }
+
+    public int column() {
+        return this.position.getColumn();
     }
 
     public void insert(@NotNull final InventoryContents contents, @NotNull final Consumer<ClickEvent> consumer) {
-        contents.set(this.row, this.column, this.clickableItem(consumer));
+        contents.set(this.row(), this.column(), this.clickableItem(consumer));
     }
 
     @NotNull
@@ -50,6 +63,28 @@ public final class FileElement {
 
     public void fill(@NotNull final InventoryContents contents, @NotNull final Consumer<ClickEvent> consumer) {
         contents.fill(this.clickableItem(consumer));
+    }
+
+    @NotNull
+    public FileElement changeColumn(final int column) {
+        return new FileElement(this.itemStack, this.row(), column);
+    }
+
+    @NotNull
+    public FileElement changeRow(final int row) {
+        return new FileElement(this.itemStack, row, this.column());
+    }
+
+    @NotNull
+    public FileElement changeItemStack(@NotNull final ItemStack itemStack) {
+        return new FileElement(itemStack, this.position);
+    }
+
+    @NotNull
+    public FileElement changeMaterial(@NotNull final Material material) {
+        final ItemStack clone = this.itemStack.clone();
+        clone.setType(material);
+        return new FileElement(clone, this.position);
     }
 
     @NotNull
@@ -82,49 +117,31 @@ public final class FileElement {
             itemMeta.setLore(finalLore);
         }
         clone.setItemMeta(itemMeta);
-        return new FileElement(clone, this.row, this.column);
-    }
-
-    @NotNull
-    public FileElement replace(@NotNull final Material material) {
-        final ItemStack clone = this.itemStack.clone();
-        clone.setType(material);
-        return new FileElement(clone, this.row, this.column);
-    }
-
-    @NotNull
-    public ItemStack getItemStack() {
-        return this.itemStack;
-    }
-
-    public int getRow() {
-        return this.row;
-    }
-
-    public int getColumn() {
-        return this.column;
+        return this.changeItemStack(clone);
     }
 
     public static class Provider implements Provided<FileElement> {
 
         @Override
-        public void set(@NotNull final FileElement fileElement, @NotNull final CfgSection section, @NotNull final String s) {
-            section.set("row", fileElement.row);
-            section.set("column", fileElement.column);
-            ((BkktSection) section).setItemStack(s, fileElement.itemStack);
+        public void set(@NotNull final FileElement fileElement, @NotNull final CfgSection section, @NotNull final String path) {
+            final String dot = GeneralUtilities.putDot(path);
+            section.set(dot + "row", fileElement.row());
+            section.set(dot + "column", fileElement.row());
+            ((BkktSection) section).setItemStack(dot + "item", fileElement.getItemStack());
         }
 
         @NotNull
         @Override
-        public Optional<FileElement> get(@NotNull final CfgSection section, @NotNull final String s) {
-            final Optional<ItemStack> itemStackOptional = ((BkktSection) section).getItemStack(s);
-            final Optional<Integer> rowOptional = section.getInteger("row");
-            final Optional<Integer> columnOptional = section.getInteger("column");
+        public Optional<FileElement> get(@NotNull final CfgSection section, @NotNull final String path) {
+            final String dot = GeneralUtilities.putDot(path);
+            final Optional<ItemStack> itemStackOptional = ((BkktSection) section).getItemStack(dot + "item");
+            final Optional<Integer> rowOptional = section.getInteger(dot + "row");
+            final Optional<Integer> columnOptional = section.getInteger(dot + "column");
             if (!itemStackOptional.isPresent() || !rowOptional.isPresent() || !columnOptional.isPresent()) {
                 return Optional.empty();
             }
             return Optional.of(
-                new FileElement(itemStackOptional.get(), rowOptional.get(), columnOptional.get()));
+                new FileElement(itemStackOptional.get(), SlotPos.of(rowOptional.get(), columnOptional.get())));
         }
 
     }
